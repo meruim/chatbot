d329e4d85a12c50b13dc4ee3cabffde23b83a18b
meruim <akatsukisasori938@gmail.com>
2024-04-10 23:53:51 +0800

    Your message

---
+++ chatbot-master/client/assets/index.js
@@ -0,0 +1,40 @@
+function sendMessage() {
+    const messageInput = document.getElementById('message');
+    const message = messageInput.value.trim();
+    //If empty no return 
+    if (message !== '') {
+        const chatLog = document.getElementById('chat-log');
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/chat');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText).message;
+
+                const userMessageContainer = document.createElement('div');
+                userMessageContainer.classList.add('message-container', 'user-message-container');
+                const userMessage = document.createElement('div');
+                userMessage.innerHTML = `<strong>You:</strong> ${message}`;
+                userMessage.classList.add('user-message');
+                userMessageContainer.appendChild(userMessage);
+                chatLog.appendChild(userMessageContainer);
+
+
+                const botMessageContainer = document.createElement('div');
+                botMessageContainer.classList.add('message-container', 'bot-message-container');
+                const botMessage = document.createElement('div');
+                botMessage.innerHTML = `<strong>Kiem:</strong> ${response}`;
+                botMessage.classList.add('bot-message');
+                botMessageContainer.appendChild(botMessage);
+                chatLog.appendChild(botMessageContainer);
+
+                chatLog.scrollTop = chatLog.scrollHeight;
+
+                document.getElementById('message').value = '';
+            }
+        };
+        xhr.send(JSON.stringify({
+            message: message
+        }));
+    }
+}
---
+++ chatbot-master/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const { closestConditionFunction } = require('./function.js');
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.post('/chat', (req, res) => {
+    const message = req.body.message.toLowerCase();
+    console.log(message);
+    let response = "Sorry, I didn't understand that.";
+
+    const closestQuestion = closestConditionFunction(message);
+
+    response = closestQuestion;
+    console.log(response);
+    
+    res.json({ message: response });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
